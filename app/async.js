fc7d1b17fc52286c9613603eb33b227758229104
akapoor <axk4789@rit.edu>
2016-06-16 23:53:54 -0400

    4 files fixed

--- app/async.js
+++ app/async.js
@@ -2,10 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+  	
+	var def = $.Deferred();
+	setTimeout(function() {
+			def.resolve(value);
+		}, 30);
+	return def.promise();
+	
   },
 
   manipulateRemoteData: function(url) {
+  	var def = $.Deferred();
+
+	$.getJSON('/data/testdata.json', function(data) {
+		var names = [];
+		$.map(data.people, function(item){
+			names.push(item.name);
+		});
+
+		def.resolve(names.sort());
+	});
+
+	return def.promise();
 
   }
 };
